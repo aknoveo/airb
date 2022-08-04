9f46bd41003e888382a5f88543e3e0b7a3a22dc5
Arkady Khamaev <Arkady.Khamaev@noveogroup.com>
2022-08-04 18:01:03 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root to: 'listings#index', as: :authenticated_root
+  end
+
+  root to: redirect('/users/sign_in')
 end
